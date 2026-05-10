78eb25615b93d3374bfa887c76a3869f1b630081
Gianina Burcea <46540761+Gyanyna20@users.noreply.github.com>
2026-05-10 18:22:49 +0100

    Add files via upload

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,16 @@
+window.alert("Hello, World!");
+var A="\"This is a \'string\'.\"", B="String assigment", C="string"; //assigning multiple variable
+//creating two variables
+var Sent1 = "This is the beginning of the string";
+var Sent2 = " and this is the end of the string";
+//displaying the variables using the document.write method
+document.write(Sent1 + Sent2);
+
+
+window.alert(B);
+document.write(A);
+document.write("This text " //cocantenating a string
+    + "is a concatenating "
+    + C
+);
+document.write(2+2); //writing a expresion
