5c0dd4ba4fdba8dc79d5bd67b4081701befd9abd
Gianina Burcea <46540761+Gyanyna20@users.noreply.github.com>
2026-05-10 18:25:39 +0100

    Add files via upload

---
+++ Project7_scope_time_function/JS/main.js
@@ -0,0 +1,29 @@
+var X = 10;
+function Add_numbers_1() {
+document.write (20 + X + "<br>") ;
+}
+function Add_numbers_2() {
+document.write (x + 100);
+}
+Add_numbers_1() ;
+Add_numbers_2() ;
+
+function Add_numbers_3() {
+var X = 10;
+document.write(20 + X + "<br>") ;
+}
+function Add_numbers_4() {
+document.write(x + 100);
+}
+Add_numbers_3() ;
+Add_numbers_4() ;
+
+function Add_numbers_5() {
+var X = 10;
+console.log(15 + X) ;
+}
+function Add_numbers_6() {
+console.log(x + 100) ;
+}
+Add_numbers_5() ;
+Add_numbers_6() ;
