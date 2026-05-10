8d07ef9cc7bf1c36595008d95e0f60cf41f28eac
Gianina Burcea <46540761+Gyanyna20@users.noreply.github.com>
2026-05-10 18:24:05 +0100

    Add files via upload

---
+++ Project2_funtions/JS/main.js
@@ -0,0 +1,5 @@
+function myFunction() {
+    var A = "This is a function "; //declaring the variables
+    A += "that i am learning about" ;//using += operator
+    document.getElementById("func").innerHTML = A; //call and display the function
+}
